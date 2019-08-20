c42fdd36952682956c3fc4f6f867ce4ab6d124da
ratnaakula <ratnasree@hotmail.com>
2019-08-19 23:30:03 -0500

    f2

--- src/main/java/com/example/demo/controller/RestWebController.java
+++ src/main/java/com/example/demo/controller/RestWebController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+//it is feature branch
 @RestController
 public class RestWebController {
 
--- src/main/java/com/example/demo/controller/WebController.java
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+// it is feature branch
 @Controller
 public class WebController {
     @RequestMapping(value="/",method = RequestMethod.GET)
