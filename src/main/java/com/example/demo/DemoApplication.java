aad9cbf310ec5b93a7a9741015df50d89149fb4f
ratnaakula <ratnasree@hotmail.com>
2019-08-19 23:28:55 -0500

    f1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//it is feature branch
 @SpringBootApplication
 public class DemoApplication {
 
